abdc4f882fe71f602eeb660f5ec79613f041783f
sarahcarter1 <sarah@keolani.com>
2024-03-24 15:04:34 -0700

    git merge

--- carter.sarah/cart.php
+++ carter.sarah/cart.php
@@ -112,33 +112,7 @@
 
 
 
-          <!-- Footer Section -->
-      <div class="footer"> 
-        <div class="footer__wrapper">
-            <div class="footer__desc">
-                <h1>FurBaby</h1>
-                <p>Located in Irvine, California</p>
-                <p id="phone">714-923-9873</p>
-            </div>
-            <div class="footer__links">
-                <a href="/" class="footer__link">FAQ</a>
-                <a href="/" class="footer__link">Delivery</a>
-                <a href="/" class="footer__link">Returns</a>
-            </div>
-        </div>
-        <div class="footer__wrapper">
-            <div class="footer__links">
-                <a href="/" class="footer__link">Blog</a>
-                <a href="/" class="footer__link">Name Generator</a>
-                <a href="/" class="footer__link">FurBaby Quiz</a>
-            </div>
-            <div class="footer__links">
-                <a href="/" class="footer__link">Privacy Policy</a>
-                <a href="/" class="footer__link">Terms and Conditions</a>
-                <a href="/" class="footer__link">Accessibility</a>
-            </div>
-        </div>
-      </div>
+     
     
 </body>
 </html>
--- carter.sarah/product_list.php
+++ carter.sarah/product_list.php
@@ -140,33 +140,7 @@
     </div>
 </div>    
 
-            <!-- Footer Section -->
-      <div class="footer"> 
-        <div class="footer__wrapper">
-            <div class="footer__desc">
-                <h1>Footer</h1>
-                <p>Location</p>
-                <p id="phone">Phone Number</p>
-            </div>
-            <div class="footer__links">
-                <a href="/" class="footer__link">Link</a>
-                <a href="/" class="footer__link">Link</a>
-                <a href="/" class="footer__link">Link</a>
-            </div>
-        </div>
-        <div class="footer__wrapper">
-            <div class="footer__links">
-                <a href="/" class="footer__link">Link</a>
-                <a href="/" class="footer__link">Link</a>
-                <a href="/" class="footer__link">Link</a>
-            </div>
-            <div class="footer__links">
-                <a href="/" class="footer__link">Link</a>
-                <a href="/" class="footer__link">Link</a>
-                <a href="/" class="footer__link">Link</a>
-            </div>
-        </div>
-      </div>
+           
     
 </body>
 </html>
